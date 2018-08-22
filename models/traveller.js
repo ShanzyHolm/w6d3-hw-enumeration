cf9fc16560422dbbb9eedb33c553797262a705be
Shannon Holmes <snholmes13@gmail.com>
2018-08-22 21:21:22 +0100

    add get journeys by type of transport

--- models/traveller.js
+++ models/traveller.js
@@ -20,9 +20,17 @@ Traveller.prototype.getModesOfTransport = function () {
   });
 };
 
-Traveller.prototype.getJourneysByTransport = function (transport) {
+// Traveller.prototype.getJourneysByTransport = function (transport) {
+//   return this.journeys.find((journey) => {
+//     return journey.transport === transport;
+//   });
+// }; //Stopped giving results after found one instance
 
-};
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport;
+  });
+}; //filters and gives all instances, unlike find method
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
 
